bde4f069b11b014ee363daa93c064c4144843320
MikielAgutu <mikiel.agutu@red-gate.com>
2019-08-27 11:06:30 +0100

    Parameterize private github urls

--- clone.cmd
+++ clone.cmd
@@ -6,6 +6,18 @@ if [%1]==[] (
   set FLYWAY_BRANCH=%1
 )
 
+if [%2]==[] (
+  set FLYWAY_MASTER_REPO_URL=https://github.com/flyway/flyway-master.git
+) else (
+  set FLYWAY_MASTER_REPO_URL=%2
+)
+
+if [%3]==[] (
+  set FLYWAY_OSSIFIER_REPO_URL=https://github.com/flyway/flyway-ossifier.git
+) else (
+  set FLYWAY_OSSIFIER_REPO_URL=%3
+)
+
 setlocal
 
 SET CURRENT_DIR=%cd%
@@ -16,9 +28,9 @@ echo ============== CLEANING
 call clean.cmd || goto :error
 
 echo ============== CLONING MASTER (Git Branch: %FLYWAY_BRANCH%)
-git clone -b %FLYWAY_BRANCH% git@github.com:flyway/flyway-master.git || goto :error
+git clone -b %FLYWAY_BRANCH% %FLYWAY_MASTER_REPO_URL% || goto :error
 echo ============== CLONING OSSIFIER
-git clone git@github.com:flyway/flyway-ossifier.git || goto :error
+git clone %FLYWAY_OSSIFIER_REPO_URL% || goto :error
 
 echo ============== CLONE SUCCESS
 cd "%CURRENT_DIR%"
--- gh.cmd
+++ gh.cmd
@@ -24,6 +24,18 @@ if [%1]==[] (
   set FLYWAY_BRANCH=%1
 )
 
+if [%2]==[] (
+  set FLYWAY_MASTER_REPO_URL=https://github.com/flyway/flyway-master.git
+) else (
+  set FLYWAY_MASTER_REPO_URL=%2
+)
+
+if [%3]==[] (
+  set FLYWAY_OSSIFIER_REPO_URL=https://github.com/flyway/flyway-ossifier.git
+) else (
+  set FLYWAY_OSSIFIER_REPO_URL=%3
+)
+
 SET CURRENT_DIR=%cd%
 
 echo ============== GH START (Git Branch: %FLYWAY_BRANCH%)
@@ -36,7 +48,7 @@ if exist flyway (
 cd "%CURRENT_DIR%"
 
 echo ============== CLONING
-call clone.cmd %FLYWAY_BRANCH% || goto :error
+call clone.cmd %FLYWAY_BRANCH% %FLYWAY_MASTER_REPO_URL% %FLYWAY_OSSIFIER_REPO_URL% || goto :error
 
 echo ============== BUILDING MASTER
 cd flyway-master
