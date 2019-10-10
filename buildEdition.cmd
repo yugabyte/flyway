bdee8ddc97ac60b56cb3a26996711c8bbbdddb7c
MikielAgutu <mikiel.agutu@red-gate.com>
2019-10-10 16:42:15 +0100

    Add buildEdition

---
+++ buildEdition.cmd
@@ -0,0 +1,77 @@
+@echo off
+
+if [%1]==[] goto :nobranch
+set FLYWAY_BRANCH=%1
+
+setlocal
+
+SET CURRENT_DIR=%cd%
+SET SETTINGS_FILE=%CURRENT_DIR%/settings.xml
+
+if "%FLYWAY_REPO_URL%"=="" (
+  set FLYWAY_REPO_URL=https://github.com/flyway/flyway
+)
+
+echo ============== BUILD EDITION START (Git Branch: %FLYWAY_BRANCH%)
+
+echo ============== CLONING
+call clone.cmd %FLYWAY_BRANCH% || goto :error
+
+echo ============= BUILDING EDITIONS
+
+call ossify.cmd
+
+echo ============== CREATING OUTPUT DIRECTORY STRUCTURE
+
+SET VERSION=0-SNAPSHOT
+
+if exist artifacts (
+  rmdir artifacts /s /q || goto :error
+)
+
+mkdir artifacts
+mkdir artifacts\community
+mkdir artifacts\pro
+mkdir artifacts\enterprise
+
+echo ============== COPYING ARTIFACTS TO OUTPUT DIRECTORY
+
+robocopy flyway\flyway-core\target artifacts\community flyway-core-%VERSION%.jar
+robocopy flyway\flyway-gradle-plugin\target artifacts\community flyway-gradle-plugin-%VERSION%.jar
+robocopy flyway\flyway-maven-plugin\target artifacts\community flyway-maven-plugin-%VERSION%.jar-%VERSION%.jar
+robocopy flyway\flyway-commandline\target artifacts\community flyway-commandline-%VERSION%.jar
+robocopy flyway\flyway-commandline\target artifacts\community flyway-commandline-%VERSION%-windows-x64.zip 
+robocopy flyway\flyway-commandline\target artifacts\community flyway-commandline-%VERSION%-linux-x64.tar.gz
+robocopy flyway\flyway-commandline\target artifacts\community flyway-commandline-%VERSION%-macosx-x64.tar.gz
+
+robocopy flyway-pro\flyway-core\target artifacts\pro flyway-core-%VERSION%.jar
+robocopy flyway-pro\flyway-gradle-plugin\target artifacts\pro flyway-gradle-plugin-%VERSION%.jar
+robocopy flyway-pro\flyway-maven-plugin\target artifacts\pro flyway-maven-plugin-%VERSION%.jar-%VERSION%.jar
+robocopy flyway-pro\flyway-commandline\target artifacts\pro flyway-commandline-%VERSION%.jar
+robocopy flyway-pro\flyway-commandline\target artifacts\pro flyway-commandline-%VERSION%-windows-x64.zip
+robocopy flyway-pro\flyway-commandline\target artifacts\pro flyway-commandline-%VERSION%-linux-x64.tar.gz
+robocopy flyway-pro\flyway-commandline\target artifacts\pro flyway-commandline-%VERSION%-macosx-x64.tar.gz
+
+robocopy flyway-enterprise\flyway-core\target artifacts\enterprise flyway-core-%VERSION%.jar
+robocopy flyway-enterprise\flyway-gradle-plugin\target artifacts\enterprise flyway-gradle-plugin-%VERSION%.jar
+robocopy flyway-enterprise\flyway-maven-plugin\target artifacts\enterprise flyway-maven-plugin-%VERSION%.jar-%VERSION%.jar
+robocopy flyway-enterprise\flyway-commandline\target artifacts\enterprise flyway-commandline-%VERSION%.jar
+robocopy flyway-enterprise\flyway-commandline\target artifacts\enterprise flyway-commandline-%VERSION%-windows-x64.zip
+robocopy flyway-enterprise\flyway-commandline\target artifacts\enterprise flyway-commandline-%VERSION%-linux-x64.tar.gz
+robocopy flyway-enterprise\flyway-commandline\target artifacts\enterprise flyway-commandline-%VERSION%-macosx-x64.tar.gz
+
+echo ============== BUILD EDITION SUCCESS
+cd "%CURRENT_DIR%"
+goto :EOF
+
+:error
+set ERRORLVL=%errorlevel%
+echo ============== BUILD EDITION FAILED WITH ERROR %ERRORLVL%
+cd "%CURRENT_DIR%"
+pause
+exit /b %ERRORLVL%
+
+:nobranch
+echo ERROR: Missing branch!
+echo USAGE: buildEdtion.cmd feature-branch
+exit /b 1
